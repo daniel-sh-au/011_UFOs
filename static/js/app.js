f343d502da7c1725ce37f005c95c28908f05c506
Daniel Au <daniel.au123@hotmail.com>
2022-07-12 19:07:34 -0400

    completed app.js (11.5.4)

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,25 @@ function buildTable(data) {
         });
     });
 }
+
+// create function to handle what to do when an input in given
+function handleClick() {
+    // grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // check if date was entered
+    if (date) {
+        // Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will just be the original tableData.
+    buildTable(filteredData);
+}
+
+// call handleClick function when filter button on html is clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
